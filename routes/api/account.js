96b9c218bee555024ef86938026defe9596df16f
boogeymanCoder <johnbryanpitcasoy@gmail.com>
2021-10-27 07:46:49 +0800

    Added account query

--- routes/api/account.js
+++ routes/api/account.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 const Account = require("../../models/account");
 
 router.post("/new", async (req, res) => {
   const account = new Account({
     username: req.body.username,
-    password: req.body.password,
+    password: await bcrypt.hash(req.body.password, 10),
     email: req.body.email,
     fullname: req.body.fullname,
     address: req.body.address,
@@ -23,6 +24,18 @@ router.post("/new", async (req, res) => {
       console.log("Account not saved, cause:", err);
       res.status(400).json(err);
     });
+});
+
+router.get("/id/:id", async (req, res) => {
+  const account = await Account.findById(req.params.id);
+
+  res.json(account);
+});
+
+router.get("/username/:username", async (req, res) => {
+  const account = await Account.findOne({ username: req.params.username });
+
+  res.json(account);
 });
 
 module.exports = router;
