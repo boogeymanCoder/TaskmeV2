d2cdbb4110e997d6f31592995f2f663710cb14c7
boogeymanCoder <johnbryanpitcasoy@gmail.com>
2021-10-27 19:45:24 +0800

    Added application api

--- routes/error.js
+++ routes/error.js
@@ -1,9 +1,11 @@
 const AccountNotFoundError = "Account Not Found";
 const OtpNotFoundError = "Otp Not Found";
 const TaskNotFoundError = "Task Not Found";
+const ApplicationNotFoundError = "Application Not Found";
 
 module.exports = {
   AccountNotFoundError: AccountNotFoundError,
   OtpNotFoundError: OtpNotFoundError,
   TaskNotFoundError: TaskNotFoundError,
+  ApplicationNotFoundError: ApplicationNotFoundError,
 };
