26610ea8583e420787c1e382778b85d282915e4c
boogeymanCoder <johnbryanpitcasoy@gmail.com>
2021-10-27 08:06:06 +0800

    Added accound updating

--- routes/api/account.js
+++ routes/api/account.js
@@ -38,6 +38,27 @@ router.get("/username/:username", async (req, res) => {
   res.json(account);
 });
 
-router.get("/");
+// TODO add api authentication
+router.patch("/:id", async (req, res) => {
+  const account = await Account.findById(req.params.id);
+  account.username = req.body.username ? req.body.username : account.username;
+  account.password = req.body.password ? req.body.password : account.password;
+  account.email = req.body.email ? req.body.email : account.email;
+  account.fullname = req.body.fullname ? req.body.fullname : account.fullname;
+  account.address = req.body.address ? req.body.address : account.address;
+  account.contact = req.body.contact ? req.body.contact : account.contact;
+  account.gender = req.body.gender ? req.body.gender : account.gender;
+
+  await account
+    .save()
+    .then(() => {
+      console.log("Account Update Successful");
+      res.json(account);
+    })
+    .catch((err) => {
+      console.log("Account update failed, cause:", err);
+      res.status(404).json(err);
+    });
+});
 
 module.exports = router;
