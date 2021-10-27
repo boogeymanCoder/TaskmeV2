dc070b6350383ff3386eac067c0ea764527d417b
boogeymanCoder <johnbryanpitcasoy@gmail.com>
2021-10-27 13:21:20 +0800

    changed otp route filename

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const accountRouter = require("./account");
 router.use("/account", accountRouter);
 
-const verificationOtpRouter =
-  require("./verificationOtp").verificationOtpRouter;
+const verificationOtpRouter = require("./otp").verificationOtpRouter;
 router.use("/verificationOtp", verificationOtpRouter);
 
-const recoveryOtpRouter = require("./verificationOtp").recoveryOtpRouter;
+const recoveryOtpRouter = require("./otp").recoveryOtpRouter;
 router.use("/recoveryOtp", recoveryOtpRouter);
+
+const taskRouter = require("./task");
+router.use("/task", taskRouter);
 
 module.exports = router;
