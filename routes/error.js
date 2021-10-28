171a9cb0d42a501f306360776e24ec0ef31ce59a
boogeymanCoder <johnbryanpitcasoy@gmail.com>
2021-10-28 10:46:37 +0800

    Finished inbox api

--- routes/error.js
+++ routes/error.js
@@ -2,10 +2,12 @@ const AccountNotFoundError = "Account Not Found";
 const OtpNotFoundError = "Otp Not Found";
 const TaskNotFoundError = "Task Not Found";
 const ApplicationNotFoundError = "Application Not Found";
+const InboxNotFoundError = "Inbox Not Found";
 
 module.exports = {
   AccountNotFoundError: AccountNotFoundError,
   OtpNotFoundError: OtpNotFoundError,
   TaskNotFoundError: TaskNotFoundError,
   ApplicationNotFoundError: ApplicationNotFoundError,
+  InboxNotFoundError: InboxNotFoundError,
 };
--- routes/inbox.js
+++ routes/inbox.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Inbox = require("../models/inbox");
+const InboxNotFoundError = require("./error").InboxNotFoundError;
 
 router.post("/", async (req, res) => {
   const inbox = new Inbox({
@@ -16,6 +17,42 @@ router.post("/", async (req, res) => {
     })
     .catch((err) => {
       console.log("Inbox Creation Failed, Cause:", err);
+      res.status(404).json(err);
+    });
+});
+
+router.get("/:owner", async (req, res) => {
+  const inbox = await Inbox.findOne({ owner: req.params.owner });
+
+  if (inbox === null) {
+    console.log(InboxNotFoundError);
+    return res.status(404).send(InboxNotFoundError);
+  }
+
+  res.json(inbox);
+});
+
+router.patch("/:owner", async (req, res) => {
+  const inbox = await Inbox.findOne({ owner: req.params.owner });
+
+  if (inbox === null) {
+    console.log(InboxNotFoundError);
+    return res.status(404).send(InboxNotFoundError);
+  }
+
+  inbox.owner = req.body.owner ? req.body.owner : inbox.owner; // FIXME is this needed?
+  inbox.conversations = req.body.conversations
+    ? req.body.conversations
+    : inbox.conversations;
+
+  await inbox
+    .save()
+    .then(() => {
+      console.log("Inbox Update Successful");
+      res.json(inbox);
+    })
+    .catch((err) => {
+      console.log("Inbox Update Failed, Cause:", err);
       res.status(404).json(err);
     });
 });
